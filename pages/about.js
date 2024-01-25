a8fca0124c25f7466b3705784b219543fa6f25f6
itc-23021 <s23021@std.it-college.ac.jp>
2024-01-25 06:59:33 +0000

    p117

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,10 @@
+import Container from 'components/container'
 import Hero from 'components/hero'
 
 export default function About () {
-  return <Hero title='about' subtitle='About development activities' />
+  return (
+    <Container>
+      <Hero title='about' subtitle='About development activities' />
+    </Container>
+  )
 }
