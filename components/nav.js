fc2b0aadb2b8f70188733186c9d14eb6de3ccd48
itc-23021 <s23021@std.it-college.ac.jp>
2024-02-20 05:31:26 +0000

    p321

--- components/nav.js
+++ components/nav.js
@@ -8,20 +8,44 @@ export default function Nav () {
   const toggleNav = () => {
     setNavIsOpen(prev => !prev)
   }
+
+  const closeNav = () => {
+    setNavIsOpen(false)
+  }
   return (
     <nav className={navIsOpen ? styles.open : styles.close}>
+      {navIsOpen && (
+        <style jsx global>
+          {`
+            @media (max-width: 767px) {
+              body {
+                overflow: hidden;
+                position: fixed;
+                width: 100%;
+              }
+            }
+          `}
+        </style>
+      )}
       <button className={styles.btn ? styles.btn : ''} onClick={toggleNav}>
-        MENU
+        <span className={styles.bar}></span>
+        <span className='sr-only'>MENU</span>
       </button>
       <ul className={styles.list}>
         <li>
-          <Link href='/'>Home</Link>
+          <Link href='/'>
+            <span onClick={closeNav}>Home</span>
+          </Link>
         </li>
         <li>
-          <Link href='/about'>About</Link>
+          <Link href='/about'>
+            <span onClick={closeNav}>About</span>
+          </Link>
         </li>
         <li>
-          <Link href='/blog'>Blog</Link>
+          <Link href='/blog'>
+            <span onClick={closeNav}>Blog</span>
+          </Link>
         </li>
       </ul>
     </nav>
